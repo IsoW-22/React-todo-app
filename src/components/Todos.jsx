cf6b6f84a6e842097d94a49b9cf0549d78884f89
IsoW-22 <isowz2000@gmail.com>
2023-05-28 01:37:19 +0330

    working on delete

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -4,17 +4,34 @@ let nextId = 0;
 
 function Todos() {
   const [todoList, SetTodoList] = useState([]);
+
   function handleAddTodo() {
     SetTodoList(
       [...todoList, { id: nextId++ }]
     );
   }
 
-  const listItems = todoList.map((todo) => <div key={todo.id} className="todo"></div>);
+  function delTodo(index) {
+    SetTodoList(
+      todoList.filter(item =>
+        item.id !== index
+      )
+    );
+    console.log(todoList);
+  }
+
+  const listItems = todoList.map((todo, index) =>
+    <div className="todo" key={index} index={index}>
+      {/* <input type="checkbox" onClick={() => markDone(index)} /> */}
+      <input type="text" />
+      <button onClick={() => delTodo(index)}>del</button>
+      <button>fav</button>
+    </div>
+  );
   return (
     <div className="body">
       <div>{listItems}</div>
-      <button onClick={handleAddTodo}>ADD TODO</button>
+      <button onClick={handleAddTodo} className="add-todo">ADD TODO</button>
     </div>
   );
 }
