d5ef0b30125b22c63d140bd43811061b962ea326
IsoW-22 <isowz2000@gmail.com>
2023-05-28 21:12:09 +0330

    lists update

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,22 +2,43 @@ import { useState } from "react";
 
 function Todos() {
   const [todoList, SetTodoList] = useState([]);
-  const [selectTodos, setSelectTodos] = useState(todoList);
-
+  const [list, SetList] = useState("all");
+  let listItems = todoList.map((todo) => (
+    <div className={todo.isDone ? "done-todo" : "todo"} key={todo.id}>
+      <input
+        type="checkbox"
+        onChange={(e) => handleMarkDone(todo.id, e.target.checked)}
+        checked={todo.isDone}
+      />
+      <input type="text" />
+      <button onClick={() => handleDeleteTodo(todo.id)}>del</button>
+      <button>fav</button>
+    </div>
+  ));
   function Lists() {
-    function handleAllTodos() {
-      SetTodoList(
-        
-      )
+    function handleDoneTodos() {
+      const newTodoList = todoList.filter(todo => todo.isDone === true);
+      listItems = newTodoList.map((todo) => (
+        <div className={todo.isDone ? "done-todo" : "todo"} key={todo.id}>
+          <input
+            type="checkbox"
+            onChange={(e) => handleMarkDone(todo.id, e.target.checked)}
+            checked={todo.isDone}
+          />
+          <input type="text" />
+          <button onClick={() => handleDeleteTodo(todo.id)}>del</button>
+          <button>fav</button>
+        </div>
+      ));
     }
-    return(
-        <ul className="lists">
-            <li>All Todos</li>
-            <li>Active Todos</li>
-            <li>Done todos</li>
-            <li>Important todos</li>
-        </ul>
-    )
+    return (
+      <ul className="lists">
+        <li>All Todos</li>
+        <li>Active Todos</li>
+        <li onClick={handleDoneTodos}>Done todos</li>
+        <li>Important todos</li>
+      </ul>
+    );
   }
 
   function handleMarkDone(index, isDone) {
@@ -30,39 +51,16 @@ function Todos() {
         }
       })
     );
-    setSelectTodos(
-      todoList.map((todo) => {
-        if (todo.id === index) {
-          return { ...todo, isDone: isDone };
-        } else {
-          return todo;
-        }
-      })
-    );
   }
 
   function handleAddTodo() {
     SetTodoList([...todoList, { id: crypto.randomUUID(), isDone: false }]);
-    setSelectTodos([...todoList, { id: crypto.randomUUID(), isDone: false }]);
   }
 
   function handleDeleteTodo(index) {
     SetTodoList(todoList.filter((item) => item.id !== index));
-    setSelectTodos(todoList.filter((item) => item.id !== index));
   }
 
-  const listItems = todoList.map((todo) => (
-    <div className={todo.isDone ? "done-todo" : "todo"} key={todo.id}>
-      <input
-        type="checkbox"
-        onChange={(e) => handleMarkDone(todo.id, e.target.checked)}
-        checked={todo.isDone}
-      />
-      <input type="text" />
-      <button onClick={() => handleDeleteTodo(todo.id)}>del</button>
-      <button>fav</button>
-    </div>
-  ));
   return (
     <>
       <div className="nav">
