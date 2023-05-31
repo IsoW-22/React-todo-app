d16e2081681707c82837e1eff48493a436296c2d
IsoW-22 <isowz2000@gmail.com>
2023-06-01 00:19:54 +0330

    fixed lists, added important todos

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,50 +2,17 @@ import { useState } from "react";
 
 function Todos() {
   const [todoList, SetTodoList] = useState([]);
-  const [list, SetList] = useState("all");
-  let listItems = todoList.map((todo) => (
-    <div className={todo.isDone ? "done-todo" : "todo"} key={todo.id}>
-      <input
-        type="checkbox"
-        onChange={(e) => handleMarkDone(todo.id, e.target.checked)}
-        checked={todo.isDone}
-      />
-      <input type="text" />
-      <button onClick={() => handleDeleteTodo(todo.id)}>del</button>
-      <button>fav</button>
-    </div>
-  ));
-  function Lists() {
-    function handleDoneTodos() {
-      const newTodoList = todoList.filter(todo => todo.isDone === true);
-      listItems = newTodoList.map((todo) => (
-        <div className={todo.isDone ? "done-todo" : "todo"} key={todo.id}>
-          <input
-            type="checkbox"
-            onChange={(e) => handleMarkDone(todo.id, e.target.checked)}
-            checked={todo.isDone}
-          />
-          <input type="text" />
-          <button onClick={() => handleDeleteTodo(todo.id)}>del</button>
-          <button>fav</button>
-        </div>
-      ));
-    }
-    return (
-      <ul className="lists">
-        <li>All Todos</li>
-        <li>Active Todos</li>
-        <li onClick={handleDoneTodos}>Done todos</li>
-        <li>Important todos</li>
-      </ul>
-    );
+  const [listState, SetListState] = useState("all");
+
+  function handleDeleteTodo(index) {
+    SetTodoList(todoList.filter((item) => item.id !== index));
   }
 
   function handleMarkDone(index, isDone) {
     SetTodoList(
       todoList.map((todo) => {
         if (todo.id === index) {
-          return { ...todo, isDone: isDone };
+          return { ...todo, isDone: isDone, isImportant: false };
         } else {
           return todo;
         }
@@ -53,12 +20,80 @@ function Todos() {
     );
   }
 
-  function handleAddTodo() {
-    SetTodoList([...todoList, { id: crypto.randomUUID(), isDone: false }]);
+  function handleImportantTodo(index, isImportant) {
+    SetTodoList(
+      todoList.map((todo) => {
+        if (todo.id === index) {
+          return { ...todo, isImportant: isImportant };
+        } else {
+          return todo;
+        }
+      })
+    );
   }
 
-  function handleDeleteTodo(index) {
-    SetTodoList(todoList.filter((item) => item.id !== index));
+  function TodoList() {
+    let todoItems;
+    function makeMap(givenList) {
+      const newList = givenList.map((todo) => (
+        <div className={todo.isDone ? "todo done" : todo.isImportant ? "important" : "todo"} key={todo.id}>
+          <input
+            type="checkbox"
+            onChange={(e) => handleMarkDone(todo.id, e.target.checked)}
+            checked={todo.isDone}
+          />
+          <input type="text" />
+          <button onClick={() => handleDeleteTodo(todo.id)}>del</button>
+          <input
+            type="checkbox"
+            onChange={(e) => handleImportantTodo(todo.id, e.target.checked)}
+            checked={todo.isImportant}
+            disabled={todo.isDone ? true : false}
+          />
+        </div>
+      ));
+      return newList;
+    }
+    switch (listState) {
+      case "done": {
+        const newTodoList = todoList.filter((item) => item.isDone === true);
+        todoItems = makeMap(newTodoList);
+        break;
+      }
+      case "active": {
+        const newTodoList = todoList.filter((item) => item.isDone === false);
+        todoItems = makeMap(newTodoList);
+        break;
+      }
+      case "important": {
+        const newTodoList = todoList.filter(
+          (item) => item.isImportant === true
+        );
+        todoItems = makeMap(newTodoList);
+        break;
+      }
+      default:
+        todoItems = makeMap(todoList);
+    }
+    return todoItems;
+  }
+
+  function Lists() {
+    return (
+      <ul className="lists">
+        <li onClick={() => SetListState("all")}>All Todos</li>
+        <li onClick={() => SetListState("active")}>Active Todos</li>
+        <li onClick={() => SetListState("done")}>Done todos</li>
+        <li onClick={() => SetListState("important")}>Important todos</li>
+      </ul>
+    );
+  }
+
+  function handleAddTodo() {
+    SetTodoList([
+      ...todoList,
+      { id: crypto.randomUUID(), isDone: false, isImportant: false },
+    ]);
   }
 
   return (
@@ -67,7 +102,7 @@ function Todos() {
         <Lists />
       </div>
       <div className="body">
-        <div>{listItems}</div>
+        <TodoList />
         <button onClick={handleAddTodo} className="add-todo">
           ADD TODO
         </button>
